b88f63fabb295d2e803dcc4a934bc56c3cca01c5
janakparajuli <janak.parajuli1@gmail.com>
2015-12-23 14:25:48 +0545

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,58 @@
-makeCacheMatrix <- function(x = matrix()) {
-inverse <- NULL
-set <- function(y){
-	x <<- y
-	inverse <<- NULL
+cacheSolve <- function(x, ...) 
+{
+if(require("corpcor")){
+	print("The loading of corpcor is done correctly")
+	} else {
+		print("Corpcor not found, so trying to install corpcor...")
+		install.packages("corpcor")
+		if(require(corpcor)){
+			print("Corpcor is installed and loaded")
+			} else {
+			stop("Corpcor could not be installed. Please try later")
+			}
+		}
+inverse <- x$getinverse()
+if(!is.null(inverse)){
+	message("The matrix is in memory")
+	return(inverse)
 	}
-get <- function() x
-setinverse <- function(Inverse) inverse <<- Inverse
-getinverse <- function() inverse
-list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+message("The matrix inverse is not in memory so the inverse is gonna be computed if possible")
+data <- x$get()
+inverse <- pseudoinverse(data, ...)
+x$setinverse(inverse)
+inverse
 }
 
 
+#Below are few experiments tried to check if it works
+#lets check for square matrix first
+x <- matrix(rpois(16, 2), nrow = 4)
+mcmx <- makeCacheMatrix(x)
+mcmx$get()
+cacheSolve(mcmx)
+cacheSolve(mcmx)
+invx <- cacheSolve(mcmx)
+
+#lets check with rectangular matrix where row > col
+y <- matrix(rpois(15, 2), nrow = 5, ncol = 3)
+mcmy <- makeCacheMatrix(y)
+mcmy$get()
+cacheSolve(mcmy)
+cacheSolve(mcmy)
+invy <- cacheSolve(mcmy)
+
+#lets check with rectangular matrix where row < col
+z <- matrix(rpois(15, 2), nrow = 3, ncol = 5)
+mcmz <- makeCacheMatrix(z)
+mcmz$get()
+cacheSolve(mcmz)
+cacheSolve(mcmz)
+invz <- cacheSolve(mcmz)
+
+#Below we check whether the multiplication of matrix and its inverse gives identity or not
+invx %*% x 
+x %*% invx
+invy %*% y
+y %*% invy
+invz %*% z
+z %*% invz 
